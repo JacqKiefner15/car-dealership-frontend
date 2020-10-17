9b2e346714331d0c32ee39ef7869a81df4e06cdf
JacqKiefner15 <jacqkiefner@gmail.com>
2020-10-17 17:34:37 -0500

    single car component changes

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,5 +1,7 @@
+import { Car } from './../shared/services/models/cars';
+import { Router } from '@angular/router';
 import { CARS } from './../mock-cars';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -8,14 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars.component.scss']
 })
 export class CarsComponent implements OnInit {
-
+@Input() car: Car
+carImg: string
   cars = CARS;
-  
 
 
-  constructor() { }
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
   }
+
+  routeToViewCar(id: number) {
+    this.router.navigate([`/cars/${id}`])
+  }
+
+
+
+
 
 }
