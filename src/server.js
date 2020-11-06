062915b94b1166c7892c72f3ff0aece3202a090b
JacqKiefner15 <jacqkiefner@gmail.com>
2020-11-06 11:09:43 -0600

    heroku changes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const forceSsl = require('force-ssl-heroku');
 const path = require('path');
-const enableProdMode = require('@angular/core').enableProdMode
-enableProdMode();
 const app = express();
-app.use(forceSsl);
-app.use(express.static('./dist/shelly-bluebook-dealership'));
-app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/dist/shelly-bluebook-dealership/index.html'))
-});
-app.listen(process.env.PORT || 8080, ()=>{
-  console.log('Server started')
-})
+app.use(express.static(__dirname + '/dist/shelly_bluebook_frontend_dealership'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/shelly_bluebook_frontend_dealership/index.html'));});
+app.listen(process.env.PORT || 8080);
