94192a35df25a6e1540522123f2791dc155d59c6
JacqKiefner15 <jacqkiefner@gmail.com>
2020-11-06 14:56:40 -0600

    server changes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shelly_bluebook_frontend_dealership'));
+app.use(express.static(__dirname + '/dist/Shelly_Bluebook_Frontend_Dealership'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/shelly_bluebook_frontend_dealership/index.html'));});
+'/dist/Shelly_Bluebook_Frontend_Dealership/index.html'));});
 app.listen(process.env.PORT || 8080);
